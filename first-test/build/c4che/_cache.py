43f79f24ffc81b2ef78cebb236322a082154f894
Stefan Akatyschew <stefan.aka@posteo.de>
2021-02-02 16:23:09 +0100

    copied first-test folder for experimentation

---
+++ first-test/build/c4che/_cache.py
@@ -0,0 +1,8 @@
+ARCHS = ['arm-rtems5']
+ARCH_BSPS = ['arm-rtems5-imx7']
+BINDIR = '/usr/local/bin'
+LIBDIR = '/usr/local/lib'
+LONG_COMMANDS = 'no'
+PREFIX = '/usr/local'
+RTEMS_TOOLS = ['/home/stefan/rtems/5/bin']
+SHOW_COMMANDS = 'no'
